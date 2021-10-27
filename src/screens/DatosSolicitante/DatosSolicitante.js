6ca6f831a411719812e4b77bdf3b85816bc73385
unknown <scordovap@unsa.edu.pe>
2021-10-26 23:41:32 -0500

    cambios interface

--- src/screens/DatosSolicitante/DatosSolicitante.js
+++ src/screens/DatosSolicitante/DatosSolicitante.js
@@ -1,23 +1,54 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Text, TextInput, ScrollView } from "react-native";
+import React, { Component, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  Image,
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import DateTimePicker from '@react-native-community/datetimepicker';
 import styles from "./DatosStyle";
 
-
-
 function DatosSolicitante(props) {
+console.log(props);
+const [date, setDate] = useState(new Date());
+const [mode, setMode] = useState('date');
+const [show, setShow] = useState(false);
+
+const onChange = (event, selectedDate) => {
+  const currentDate = selectedDate || date;
+  setShow(Platform.OS === 'ios');
+  setDate(currentDate);
+};
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.titulo}>Autorización de trabajo</Text>
       <View style={styles.contenedorData}>
-        <Text style={styles.subtitulo}>Datos del Solicitante</Text>
+        <Text style={styles.subtitulo}>Datos por parte del Solicitante</Text>
         <Text style={styles.supervisorAcargo}>
           Supervisor de Marcobre Responsable
         </Text>
         <TextInput
           placeholder="Ingrese nombre del supervisor"
-          maxLength={140}
           multiline={false}
           spellCheck={true}
           textBreakStrategy="simple"
@@ -27,8 +58,9 @@ function DatosSolicitante(props) {
         ></TextInput>
         <Text style={styles.nCelular}>N° Celular</Text>
         <TextInput
+          keyboardType="numeric" 
           placeholder="Ingrese celular del supervisor"
-          maxLength={140}
+          maxLength={12}
           multiline={false}
           spellCheck={true}
           textBreakStrategy="simple"
@@ -42,15 +74,15 @@ function DatosSolicitante(props) {
             editable={false}
             style={styles.textInput}
           ></TextInput>
-          <FontAwesomeIcon
+         <TouchableOpacity onPress={showDatepicker}>
+           <FontAwesomeIcon
             name="calendar-times-o"
             style={styles.iconoFecha}
-          ></FontAwesomeIcon>
+          ></FontAwesomeIcon></TouchableOpacity>
         </View>
         <Text style={styles.supervisorAcargo1}>Supervisor Ejecutante</Text>
         <TextInput
           placeholder="Ingrese nombre del supervisor"
-          maxLength={140}
           multiline={false}
           spellCheck={true}
           textBreakStrategy="simple"
@@ -66,10 +98,10 @@ function DatosSolicitante(props) {
             editable={false}
             style={styles.fechaInicioInput}
           ></TextInput>
-          <MaterialCommunityIconsIcon
+          <TouchableOpacity onPress={showTimepicker}><MaterialCommunityIconsIcon
             name="calendar-multiple"
             style={styles.iconoFecha1}
-          ></MaterialCommunityIconsIcon>
+          ></MaterialCommunityIconsIcon></TouchableOpacity>
         </View>
         <Text style={styles.fechaFin}>Fecha Fin</Text>
         <View style={styles.fechaFinInputStack}>
@@ -78,11 +110,12 @@ function DatosSolicitante(props) {
             editable={false}
             style={styles.fechaFinInput}
           ></TextInput>
-          <MaterialCommunityIconsIcon
+          
+        </View>
+        <TouchableOpacity onPress={showDatepicker}><MaterialCommunityIconsIcon
             name="calendar-multiple-check"
             style={styles.iconoFecha2}
-          ></MaterialCommunityIconsIcon>
-        </View>
+          ></MaterialCommunityIconsIcon></TouchableOpacity>
         <Text style={styles.descripcionDelTrabajo}>
           Descripción del trabajo
         </Text>
@@ -102,8 +135,17 @@ function DatosSolicitante(props) {
         ></TextInput>
         <Text style={styles.permisos}>Permisos que se requieren</Text>
         <View style={styles.permisosBox}></View>
-      </View>
-    </ScrollView>
+        {show && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={onChange} />
+            )}
+        </View>
+       </ScrollView>
   );
 }
 
